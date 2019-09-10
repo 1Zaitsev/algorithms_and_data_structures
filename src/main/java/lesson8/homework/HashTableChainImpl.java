4e6208678872ccd215e15dacbe4fd117481f9d41
qdo_ln <4yu@bk.ru>
2019-09-10 18:24:23 +0300

    Algorithms and data structure. Lesson8. Homework.

---
+++ src/main/java/lesson8/homework/HashTableChainImpl.java
@@ -0,0 +1,139 @@
+package lesson8.homework;
+
+import java.util.LinkedList;
+import lesson8.classwork.HashTable;
+
+public class HashTableChainImpl<K, V> implements HashTable<K, V> {
+
+    private static class Node<K, V> implements Entry<K, V>{
+        private K key;
+        private V value;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public K getKey() {
+            return key;
+        }
+
+        @Override
+        public void setKey(K key) {
+            this.key = key;
+        }
+
+        @Override
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "key=" + key +
+                    ", value=" + value +
+                    '}';
+        }
+    }
+
+    private int size;
+    private LinkedList<Node> [] data;
+
+    @SuppressWarnings("unchecked")
+    public HashTableChainImpl(int size) {
+        this.size = size;
+        this.data = new LinkedList[size];
+    }
+
+    private int hashFunc(K key) {
+        return key.hashCode() % data.length;
+    }
+
+    @Override
+    public boolean put(K key, V value) {
+        int index = hashFunc(key);
+        if(data[index] == null){
+            data[index] = new LinkedList<Node>();
+        }
+        data[index].add(new Node(key, value));
+        size++;
+        return true;
+    }
+
+    @Override
+    public V get(K key) {
+        int index = hashFunc(key);
+        LinkedList<Node> currentList = data[index];
+
+        for (Node node: currentList) {
+            if (node.getKey().equals(key)) {
+                return (V) node.getValue();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean contains(K key) {
+        return get(key) != null;
+    }
+
+    @Override
+    public V remove(K key) {
+        int index = hashFunc(key);
+        LinkedList<Node> currentList = data[index];
+
+        for (Node node: currentList) {
+            if (node.getKey().equals(key)) {
+                Node<K, V> result = node;
+                node = null;
+                size--;
+                return result.getValue();
+            }
+        }
+        if(currentList.isEmpty())
+            data[index] = null;
+        return null;
+    }
+
+    @Override
+    public void display() {
+        System.out.println("__________________");
+        for (int i = 0; i < data.length; i++) {
+            if(data[i] != null)
+            displayList(data[i]);
+            else
+                System.out.println("-------------");
+        }
+        System.out.println("__________________");
+    }
+
+    public void displayList(LinkedList<Node> linkedList){
+        for (Node node: linkedList) {
+            System.out.print(node + " \t");
+        }
+        System.out.println();
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;             //Elements are LinkedLists, that can't be full.
+    }
+}
---
+++ src/main/java/lesson8/homework/HomeWork8.java
@@ -0,0 +1,43 @@
+package lesson8.homework;
+
+import lesson8.classwork.HashTable;
+import lesson8.classwork.ItemDefinition;
+
+public class HomeWork8 {
+    public static void main(String[] args) {
+        HashTable<ItemDefinition, Integer> hTChain= new HashTableChainImpl<>(10);
+
+        ItemDefinition apple = new ItemDefinition(49, "Apple");
+        ItemDefinition pear = new ItemDefinition(68, "Pear");
+        ItemDefinition orange = new ItemDefinition(31, "Orange");
+        ItemDefinition lemon = new ItemDefinition(43, "Lemon");
+        ItemDefinition grapefruit = new ItemDefinition(74, "Grapefruit");
+        ItemDefinition melon = new ItemDefinition(772, "Melon");
+        ItemDefinition watermelon = new ItemDefinition(66, "Watermelon");
+        ItemDefinition kiwi = new ItemDefinition(77, "Kiwi");
+        ItemDefinition strawberry = new ItemDefinition(99, "Strawberry");
+        ItemDefinition mango = new ItemDefinition(19, "Mango");
+        ItemDefinition grapes = new ItemDefinition(38, "Grapes");
+        ItemDefinition cherry = new ItemDefinition(197, "Cherry");
+        ItemDefinition coconut = new ItemDefinition(495, "Coconut");
+        ItemDefinition papaya = new ItemDefinition(499, "Papaya");
+
+        hTChain.put(apple, 120);
+        hTChain.put(pear, 150);
+        hTChain.put(orange, 250);
+        hTChain.put(lemon, 100);
+        hTChain.put(grapefruit,500);
+        hTChain.put(melon, 50);
+        hTChain.put(kiwi, 75);
+        hTChain.put(strawberry, 750);
+        hTChain.put(mango, 175);
+        hTChain.put(grapes, 215);
+        hTChain.put(cherry, 75);
+        hTChain.put(coconut, 450);
+        hTChain.put(papaya, 400);
+
+        System.out.println("Orange costs " + hTChain.get(orange));
+
+        hTChain.display();
+    }
+}
